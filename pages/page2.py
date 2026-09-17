d965c59d07270c3450b5d1b12e5f7ff2ba0f9f6f
dylan-travers <dylan.travers3572@laposte.net>
2026-09-17 16:46:06 +0200

    page2

--- pages/page2.py
+++ pages/page2.py
@@ -1,9 +1,41 @@
-from dash import html, register_page
+from dash import Dash, dcc, html, Input, Output, callback, register_page
+import plotly.express as px
 
-register_page(__name__, name='Page 2')
+register_page(__name__, name='Cartographie des élections')
+
+df = px.data.election() 
+geojson = px.data.election_geojson()
 
 layout = html.Div([
 
-    html.P('Content of page 2')
+    html.H4('Political candidate voting pool analysis'),
+
+    html.P("Select a candidate:"),
+
+    dcc.RadioItems(
+        id='candidate', 
+        options=["Joly", "Coderre", "Bergeron"],
+        value="Coderre",
+        inline=True
+    ),
+
+    dcc.Graph(id="graph")
 
 ])
+
+@callback(
+    Output("graph", "figure"), 
+    Input("candidate", "value"))
+def display_choropleth(candidate):
+
+    fig = px.choropleth(
+        df, geojson=geojson, color=candidate,
+        locations="district", featureidkey="properties.district",
+        projection="mercator", range_color=[0, 6500])
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    return fig
+
+if __name__ == '__main__':
+    app.run(debug=True)
